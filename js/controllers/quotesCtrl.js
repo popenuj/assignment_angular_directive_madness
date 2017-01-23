3c0336e836e0a90a741a729eb1fc63ca8d4e41c6
Pramod Jacob <psjacob68@gmail.com>
2017-01-23 16:58:01 -0500

    finished creating & listing quotes

--- js/controllers/quotesCtrl.js
+++ js/controllers/quotesCtrl.js
@@ -3,11 +3,25 @@ DirectiveMadness.controller('QuotesCtrl',
   ['$scope',
     function($scope) {
       $scope.quotes = [];
+
       $scope.addQuote = function(quoteForm) {
+        // Add to quotes array
         $scope.quotes.push({
           message: quoteForm.message,
           author: quoteForm.author
         });
+        // Clear fields
+        angular.element('input[type=text]').val('');
+      }
+
+      $scope.deleteQuote = function(quote) {
+        // Delete from quotes array 
+        $scope.quotes.forEach(function(el, i) {
+          if (el === quote) {
+            $scope.quotes.splice(i, 1);
+            return;
+          }
+        })
       }
     }
   ]
